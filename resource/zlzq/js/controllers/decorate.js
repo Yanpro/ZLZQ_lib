f4b9f3dfe75e151a025ceb5e25bcccf22ed9757e
liuqiang <294007813@qq.com>
2015-07-03 15:58:57 +0800

    装修

--- resource/zlzq/js/controllers/decorate.js
+++ resource/zlzq/js/controllers/decorate.js
@@ -4,52 +4,63 @@ define(['BaseView', "cUIInputClear","cUIImageSlider" ,"Model", "Store","text!Tpl
     var View = BaseView.extend({
         ViewName: 'comment',
         events: {
-            "click .housing .btn":"toReserve",
-            "click .location_icon" :"toLocation",
-            "click .search-btn":"toSearch",
-            "click .order-list .btn":"toSubscribe",
+            "click .house-list li": "toOrderDetail",
             "click .bottom-bar .rent":"toRent",
             "click .bottom-bar .mine":"toPersonal",
             "click .bottom-bar .order":"toOrder",
-            //"click .bottom-bar .schedule":"toSchedule"
+            "click .bottom-bar .schedule":"toSchedule",
+            "click .search-icon":"toSearch",
         },
-        changeTab: function (e) {
-            var target = $(e.currentTarget),
-                 icon=target.find(".b-icon"),
-                tab = target.data("key");
-            icon.toggleClass("active");
-        },
-        toSubscribe:function(e){
-            Lizard.goTo("subscribe.html");
-        },
-        toReserve:function(e){
-            self.$el.find(".info_ct").hide();
-            self.$el.find(".housing").hide();
-            self.$el.find(".reserve_ct").show();
-        },
+
         ajaxException: function (msg) {
-            self.loginBtn.html("登录");
             self.hideLoading();
             self.showMyToast('网络错误，请重试', 2000);
         },
         onCreate: function () {
             self = this;
-            // self.$el.html(TplHouse);
 
         },
 
-        onShow: function () {
-            self.setHeader();
+        getList:function(callback){
+            var url=Lizard.host+Lizard.apiUrl+"companies",
+                paras={},
+                method="get";
+            var orderDate=new Array(2);
+            $.ajax({
+                url: url,
+                dataType: "json",
+                type: method,
+                data:paras,
+                success: function (data) {
+                    self.hideLoading();
 
-            self.$el.html(TplDecorate);
+                    for(var i=0;i<data.realties.length;i++){
+                        data.realties[i].state=data.orders[i].state;
+                    }
+                    //self.$el.html(_.template(TplOrderList, {list: data.realties}));
+
+                },
+                error: function (e) {
+                    self.showMyToast("服务器异常", 1000);
+                    self.hideLoading();
+                }
+            });
+        },
+
+        onShow: function () {
+            $("#headerview").hide();
+            self.setHeader();
+            //self.$el.html(TplDecorate);
             self.hideLoading();
+            self.getList();
+
 
 
         },
         //设置标题
         setHeader: function () {
             self.header.set({
-                title: '装修',
+                title: '装修公司',
                 back: true,
                 backtext: '<i class="icon-back "></i> ',
                 view: this,
